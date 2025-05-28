201d56787477a53d920f06003ca7692d019d495d
feargodface <178maks@gmail.com>
2025-05-28 11:28:46 +0700

    Added password generator

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+def generate_password(length=12):
+    #Генерирует случайный пароль из букв, цифр и специальных символов.
+
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+print("Generated Password:", generate_password())

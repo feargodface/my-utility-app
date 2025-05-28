1ec069c2b60f48fe6022de8707d716f8372e4080
feargodface <178maks@gmail.com>
2025-05-28 11:31:16 +0700

    Added temperature converter

---
+++ main.py
@@ -0,0 +1,12 @@
+def celsius_to_fahrenheit(celsius):
+    #Конвертирует температуру из Цельсия в Фаренгейт.
+
+    return (celsius * 9/5) + 32
+
+def fahrenheit_to_celsius(fahrenheit):
+    #Конвертирует температуру из Фаренгейта в Цельсий.
+
+    return (fahrenheit - 32) * 5/9
+
+print("100°C в Fahrenheit:", celsius_to_fahrenheit(100))
+print("212°F в Celsius:", fahrenheit_to_celsius(212))
